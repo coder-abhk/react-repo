924fa5b752098b7161be0189b1cff6f7eba23a38
coder-abhk <abhkrwt07@gmail.com>
2021-01-17 18:27:38 +0530

    counter-app using useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const handleIncreaseClick = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecreaseClick = () => {
+    return count > 0 ? setCount(count - 1) : null;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container p-3 d-flex align-items-center flex-column">
+    <header>
+        <h1>Counter App</h1>
+    </header>
+      <div className="badge bg-danger text-white p-2 m-3 ">{count}</div>
+      <div className="container d-flex justify-content-center align-items-center">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button
+            className="btn btn-outline-primary btn-sm m-2"
+            onClick={handleIncreaseClick}
+          >
+            increase
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <button
+            className="btn btn-outline-secondary btn-sm"
+            onClick={handleDecreaseClick}
+          >
+            decrese
+          </button>
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
