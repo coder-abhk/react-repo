608edd6c55c016461b25e7842d1cbd0b2a0f1790
coder-abhk <abhkrwt07@gmail.com>
2021-01-22 17:20:44 +0530

    todo app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,80 @@
 import React, { useState } from "react";
 
 function App() {
-  let [todos, setTodos ] = useState([{
-    todo: 'hello'
-  }])
+  // pre todos
+  let [todos, setTodos] = useState([
+    {
+      id: "1",
+      title: "Shine",
+      phrase: "Shine like a daimond.",
+    },
+    {
+      id: "2",
+      title: "Rise",
+      phrase: "Rise and shine.",
+    },
+  ]);
 
-  let handleSubmit = (e) => {};
+  // input data set
+  let [title, setTitle] = useState("");
+  let [phrase, setPhrase] = useState("");
+  let [id, setId] = useState("");
 
+  // show data when imput data
   let handleChange = (e) => {
     document.getElementById("model-view").textContent = e.target.value;
+  };
+
+  // action on input data submit
+  let handleSubmit = (e) => {
     e.preventDefault();
+    let newTodo = { id, phrase, title };
+    setTodos([...todos, newTodo]);
+    setId("");
+    setPhrase("");
+    setTitle("");
   };
 
   return (
-    <div className="container p-3">
+    <div className="container-sm p-3">
       <header>
-        <h1 className="text-secondary text-center">X-Todos</h1>
+        <h1 className="text-secondary text-center">x-Todos</h1>
       </header>
       <div className="form-group">
+        {/* Todo form */}
         <form onSubmit={handleSubmit}>
-          <label className="text-secondary p-2" hmtlfor="todo">
-            Todo
-          </label>
+          <label className="text-secondary p-2">Title</label>
           <input
             type="text"
-            name="todo"
+            className="form-control"
+            placeholder="title"
+            value={title}
+            onChange={(e) => {
+              handleChange(e);
+              setTitle(e.target.value);
+            }}
+          />
+          <label className="text-secondary p-2">Phrase</label>
+          <input
+            type="text"
             className="form-control"
             placeholder="type here..."
-            onChange={handleChange}
+            value={phrase}
+            onChange={(e) => {
+              handleChange(e);
+              setPhrase(e.target.value);
+            }}
+          />
+          <label className="text-secondary p-2">Id(required)</label>
+          <input
+            type="text"
+            value={id}
+            className="form-control mt-2"
+            placeholder="you id..."
+            onChange={(e) => {
+              setId(e.target.value);
+              handleChange(e);
+            }}
           />
           <p className="p-2 text-secondary" id="model-view"></p>
           <input
@@ -38,7 +85,18 @@ function App() {
           />
         </form>
       </div>
-      <div>{todos.map((todo) => console.log(todo))}</div>
+      {/* all todos */}
+      <div className="mt-4 mb-2">
+        {todos.map((todo) => (
+          <div
+            key={todo.id}
+            className="d-block rounded-3 border border-2 border-info mb-2 p-2"
+          >
+            <h1 className="text-dark">{todo.title}</h1>
+            <p className="text-secondary">{todo.phrase}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
