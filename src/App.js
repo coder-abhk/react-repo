d3f52c4fb369de536e7415e60957b68ca236254a
coder-abhk <abhkrwt07@gmail.com>
2021-01-22 13:43:15 +0530

    src modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  let [todos, setTodos ] = useState([{
+    todo: 'hello'
+  }])
+
+  let handleSubmit = (e) => {};
+
+  let handleChange = (e) => {
+    document.getElementById("model-view").textContent = e.target.value;
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container p-3">
+      <header>
+        <h1 className="text-secondary text-center">X-Todos</h1>
       </header>
+      <div className="form-group">
+        <form onSubmit={handleSubmit}>
+          <label className="text-secondary p-2" hmtlfor="todo">
+            Todo
+          </label>
+          <input
+            type="text"
+            name="todo"
+            className="form-control"
+            placeholder="type here..."
+            onChange={handleChange}
+          />
+          <p className="p-2 text-secondary" id="model-view"></p>
+          <input
+            className="btn btn-info text-light"
+            type="submit"
+            name="submit"
+            value="Add Now"
+          />
+        </form>
+      </div>
+      <div>{todos.map((todo) => console.log(todo))}</div>
     </div>
   );
 }
